90d0ff92cedb0137f869e2e3c0c5962629e3959d
Francisco Arenas <wook82@gmail.com>
2021-04-25 19:22:44 -0400

    se sube llamada a pong

--- ping.go
+++ ping.go
@@ -2,21 +2,44 @@ package main
 
 import (
 	"encoding/json"
+	"io/ioutil"
 	"log"
 	"net/http"
 
 	"github.com/gorilla/mux"
 )
 
+type Response struct {
+	Message string `json:"message"`
+}
+
 // App export
 type App struct {
 	Router *mux.Router
 }
 
+func callPongService() string {
+	var responseObj Response
+	response, err := http.Get("http://localhost:8081/pong")
+
+	if err != nil {
+		log.Print(err.Error())
+	}
+
+	responseData, err := ioutil.ReadAll(response.Body)
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	json.Unmarshal(responseData, &responseObj)
+	return responseObj.Message
+}
+
 func pingHandler(w http.ResponseWriter, r *http.Request) {
 	log.Println("pingHandler - request")
 	var response map[string]interface{}
-	json.Unmarshal([]byte(`{ "message": "ping" }`), &response)
+	str := callPongService()
+	json.Unmarshal([]byte("{ \"send\": \"ping\", \"respond\": \""+str+"\" }"), &response)
 	respondWithJSON(w, http.StatusOK, response)
 }
 
