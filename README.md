260f95222cf2a1ec03adc2c55367861759cabd5c
w00k <wook82@gmail.com>
2021-04-27 14:37:56 -0400

    se agregan comentarios, README.md y rescate a la variable de entorno

--- README.md
+++ README.md
@@ -1,2 +1,33 @@
-# ping
-ping microservice how call a pong microservice, this will be deployed on kubernetes ... 
+# Servicio Ping
+
+Servicio **ping** que llama al servicio **pong**, para entregar una respuesta. Ambos servicios serán deployados en kubernetes. 
+
+## Pre-requisiros
+
+Tener instalado: 
+* Golang. 
+* Descargar las dependencias. 
+
+En la ruta donde está el proyecto 
+```bash 
+go mod download
+```
+
+* Agergar en las variables de entorno la URL_PONG con valor **http://localhost:8081/pong**. 
+
+Como ejemplo en Windows, abrir cmd y ejecutar. 
+```bash 
+set URL_PONG=http://localhost:8081/pong
+```
+
+## Ejecutar
+
+Solo para ejecutar 
+```bash 
+go run ping.go
+```
+
+Para compilar en Windows
+```bash 
+go build -o path/ping.exe ping.go
+```
--- ping.go
+++ ping.go
@@ -5,12 +5,29 @@ import (
 	"io/ioutil"
 	"log"
 	"net/http"
+	"os"
 
 	"github.com/gorilla/mux"
 )
 
+// ConstantApp ::: estructura que almacena variables de entorno
+type ConstantApp struct {
+	EndpointPong string
+}
+
+// Response ::: estructura de respuesta del servicio pong
 type Response struct {
 	Message string `json:"message"`
+}
+
+// constantApp ::: inicialización de variable
+var constantApp ConstantApp
+
+// función que se ejecuta al inicio, setea la variable de entorno en el struct constantApp
+func init() {
+	constantApp = ConstantApp{
+		EndpointPong: os.Getenv("URL_PONG"),
+	}
 }
 
 // App export
@@ -18,23 +35,32 @@ type App struct {
 	Router *mux.Router
 }
 
+// callPongService ::: llamada al servicio pong
+// antes de ejecutar el servicio es necesario setear la variable de entorno URL_PONG
+// en windows con cmd se hace de esta forma
+// set URL_PONG=http://localhost:8081/pong
 func callPongService() string {
 	var responseObj Response
-	response, err := http.Get("http://localhost:8081/pong")
+	response, err := http.Get(constantApp.EndpointPong)
+	// set URL_PONG=http://localhost:8081/pong
+	//response, err := http.Get("http://localhost:8081/pong")
 
 	if err != nil {
 		log.Print(err.Error())
+		return "error al llamar servicio pong"
 	}
 
 	responseData, err := ioutil.ReadAll(response.Body)
 	if err != nil {
 		log.Fatal(err)
+		return "error al leer el mensaje de servicio pong"
 	}
 
 	json.Unmarshal(responseData, &responseObj)
 	return responseObj.Message
 }
 
+// pingHandler ::: retorna la respuesta del servicio ping + la repsuesta de pong
 func pingHandler(w http.ResponseWriter, r *http.Request) {
 	log.Println("pingHandler - request")
 	var response map[string]interface{}
@@ -43,23 +69,27 @@ func pingHandler(w http.ResponseWriter, r *http.Request) {
 	respondWithJSON(w, http.StatusOK, response)
 }
 
+// respondWithJSON ::: agrega la cabecera de tipo JSON en la respuesta de ping
 func respondWithJSON(w http.ResponseWriter, code int, payload interface{}) {
 	w.Header().Set("Content-Type", "application/json")
 	w.WriteHeader(code)
 	json.NewEncoder(w).Encode(payload)
 }
 
+// initialiseRoutes ::: contiene el controlador y path de servicio ping
 func (app *App) initialiseRoutes() {
 	log.Println("start")
 	app.Router = mux.NewRouter()
 	app.Router.HandleFunc("/ping", pingHandler)
 }
 
+// run ::: levanta el servidor en puerto indicado
 func (app *App) run() {
 	log.Println("ping service on 8080")
 	log.Fatal(http.ListenAndServe(":8080", app.Router))
 }
 
+// main ::: inicio del servicio ping
 func main() {
 	app := App{}
 	app.initialiseRoutes()
